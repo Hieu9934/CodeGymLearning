a25ed7724b6ade117d0bf73d4aea282120308dd9
Hieu9934 <43hieu1999@gmail.com>
2024-11-25 20:47:07 +0700

    Finish Unit 5 Exercise 2

---
+++ Unit5Exercise2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Danh sach so nguyen to nho hon 100:");
+            for (int i = 2; i < 100; i++)
+            {
+                bool isPrimeNumber = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrimeNumber = false;
+                    }
+                }
+
+                if (isPrimeNumber)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
