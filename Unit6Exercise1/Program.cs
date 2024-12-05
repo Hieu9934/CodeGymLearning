8285a69741eb7eee61a70d7189cf8657c154cefe
Hieu9934 <43hieu1999@gmail.com>
2024-12-05 07:02:13 +0700

    Finish Unit 6 Exercise 1

---
+++ Unit6Exercise1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Input the starting number: ");
+        Int32.TryParse(Console.ReadLine(), out int number);
+        int count = 0;
+        while (count < 20)
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine(number);
+                count++;
+            }
+            number++;
+        }
+    }
+
+    public static bool IsPrime(int number)
+    {
+        for (double i = 2.0; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
